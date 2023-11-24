18f882956d388fc7426677066c179076cf841322
Kylian Janssens <kylianjanssens@hotmail.com>
2023-11-24 15:42:14 +0100

    Assync patterns Blocking code

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const _ = require('lodash');
+const http = require('http')
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+const server = http.createServer((req,res)=>{
+if (req.url === '/') {
+  res.end('Home page')
+}
+if (req.url === '/about') {
+  // BLOCKING CODE!!!
+  for(let i = 0; i < 1000; i++){
+    for(let j = 0; j < 1000; j++){
+console.log(`${i} ${j}`)
+    }
+  }
+  res.end('About page')
+}
+res.end('Error page')
+})
+
+server.listen(5000, () => {
+  console.log('server is listening on port 5000')
+})
